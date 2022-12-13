4a71987f187327f00831c2af14c4765e1570c6b2
Pandarhist <kazimov.nikita1@mail.ru>
2022-12-14 00:52:50 +0300

    Added db context, default connection.

---
+++ Code/CourseWork/MusicShop/DbContexts/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicShop.Models;
+
+namespace MusicShop.DbContexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
